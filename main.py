9e939b5883d4f64375bda9ee7bb743ceb6df6070
Peter Khalil <rikokamil97@gmail.com>
2023-11-23 22:45:46 +0200

    Test

--- main.py
+++ main.py
@@ -2,16 +2,20 @@ from flask import Flask, jsonify,request
 from gpt4all import Embed4All
 
 app = Flask(__name__)
-@app.route('/api/embedding', methods=['POST'])
-def create_Embedding():
-    try:
-        data = request.get_json()
-        text = data['text']
-        embedder = Embed4All()
-        embeddings = embedder.embed(text)
-        return jsonify({'embedding': [embeddings]})
-    except:
-        return jsonify({'error': 'Error during embedding'})
+# @app.route('/api/embedding', methods=['POST'])
+# def create_Embedding():
+#     try:
+#         data = request.get_json()
+#         text = data['text']
+#         embedder = Embed4All()
+#         embeddings = embedder.embed(text)
+#         return jsonify({'embedding': [embeddings]})
+#     except:
+#         return jsonify({'error': 'Error during embedding'})
+
+@app.route('/api/test', methods=['GET'])
+def test():
+    return jsonify({'test': 'Hello World!'})
     
 if __name__ == '__main__':
     app.run()
